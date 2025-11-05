3caa4f5947d83a02e393c5a246b02b5cf7dc1f9f
Vo Huu Phuoc <118438452+Pinnnndz@users.noreply.github.com>
2025-11-05 11:56:22 +0700

    tampermonkey.user.js

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,1 +1,15 @@
-
+// ==UserScript==
+// @name         Auto Celebrity V3 (HP ĐÃ FIX CÁC LỖI)
+// @namespace    http://tampermonkey.net/
+// @version      3.0
+// @description  Phiên bản được sửa lại với giao diện hiện đại và nhiều tiện ích khác nhau
+// @author       Huu Phuoc
+// @require      https://raw.githubusercontent.com/Pinnnndz/locket-autocelebrity-v3/main/auto-celeb.main.js
+// @homepageURL  https://github.com/Pinnnndz/locket-autocelebrity-v3
+// @downloadURL  https://raw.githubusercontent.com/Pinnnndz/locket-autocelebrity-v3/main/tampermonkey.user.js
+// @updateURL    https://raw.githubusercontent.com/Pinnnndz/locket-autocelebrity-v3/main/tampermonkey.user.js
+// @match        https://locket.binhake.dev/*
+// @run-at       document-idle
+// @grant        GM_addStyle
+// @icon         https://sf-static.upanhlaylink.com/img/image_20251102111a21dc979eb66b992f06f830ebf41f.jpg
+// ==/UserScript==
